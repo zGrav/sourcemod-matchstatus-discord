b1852577c5a5fb0f8806d1e14be8ce2d7eef2525
David Silva <zgrav@null.net>
2019-04-07 16:20:02 +0200

    change build.sh path

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-~/Desktop/sm/addons/sourcemod/scripting/spcomp -i ~/Desktop/sm/addons/sourcemod/scripting/include $PWD/src/sourcemod-matchstatus-discord.sp
+~/sm/addons/sourcemod/scripting/spcomp -i ~/sm/addons/sourcemod/scripting/include $PWD/src/sourcemod-matchstatus-discord.sp
 mv sourcemod-matchstatus-discord.smx ./bin
